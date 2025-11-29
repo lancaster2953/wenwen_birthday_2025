6625ce339cad5d165d8ef8c2c4e5902f8abce94c
lancaster2953 <80279524+lancaster2953@users.noreply.github.com>
2025-11-28 22:56:41 -0700

    Create snake.js

---
+++ snake.js
@@ -0,0 +1,7 @@
+const Snake = {
+  position: {x: 0, y: 0},
+  velocity: {x: 1, y: 0},
+  draw(context){
+    context.fillRect(this.position.x * CELL_SIZE, this.position.y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+  }
+}
